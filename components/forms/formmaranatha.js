fcb1ebddb124e611c97206dec38bfc86f2ec1f96
Ninho12 <joaopaulojpsp@gmail.com>
2022-12-23 14:43:30 -0300

    Atualização dos forms 2

---
+++ components/forms/formmaranatha.js
@@ -0,0 +1,11 @@
+import Style from "../../styles/form.module.css"
+
+export default function FormMaranatha() {
+    return (
+      <form className={Style.pequeno} >
+
+       
+      </form>
+    )
+}
+  
