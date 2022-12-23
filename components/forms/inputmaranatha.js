e413536b171b74af04d07fbce4669080d68e23f7
Ninho12 <joaopaulojpsp@gmail.com>
2022-12-22 21:38:20 -0300

    Segundo Commit

---
+++ components/forms/inputmaranatha.js
@@ -0,0 +1,9 @@
+export default function InputMaranatha() {
+    return (
+      <div>
+        <label>Nome:</label>
+        <input type="text" name="Nome" placeholder="Nome" />
+      </div>
+    )
+}
+  
