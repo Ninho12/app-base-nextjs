7c7bccc96cb32e52b85940c775e7d696a5a28553
Ninho12 <joaopaulojpsp@gmail.com>
2022-12-23 14:43:13 -0300

    atualização dos forms

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import { Inter } from '@next/font/google'
+import FormMaranatha from '../components/forms/formmaranatha'
 import InputMaranatha from '../components/forms/inputmaranatha'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -7,8 +8,11 @@ export default function Home() {
   return (
     <>
       <main>
+
         <h1> Meu Nome é João Paulo</h1>
-        <InputMaranatha></InputMaranatha>
+        <FormMaranatha>
+            <InputMaranatha></InputMaranatha>
+        </FormMaranatha>
       </main>
     </>
   )
